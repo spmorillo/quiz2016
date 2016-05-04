7528399f72858eaf2d0ea6951755ab113c4d8f98
spmorillo <sergipm11@hotmail.com>
2016-05-04 18:09:55 +0200

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -116,3 +116,16 @@ exports.update = function(req, res, next) {
       next(error);
     });
 };
+
+// DELETE /quizzes/:id
+exports.destroy = function(req, res, next) {
+  req.quiz.destroy()
+    .then( function() {
+	  req.flash('success', 'Quiz borrado con éxito.');
+      res.redirect('/quizzes');
+    })
+    .catch(function(error){
+	  req.flash('error', 'Error al editar el Quiz: '+error.message);
+      next(error);
+    });
+};
