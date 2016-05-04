f9722a06f06f7e26a805ed37b5df5abf351dd54a
spmorillo <sergipm11@hotmail.com>
2016-05-04 15:46:41 +0200

    Autoloading

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,19 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+router.get('/author', function(req, res, next) {
+  res.render('author');
+});
+
+// Autoload de rutas que usen :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
 
 
 // Definición de rutas de /quizzes
 router.get('/quizzes',                     quizController.index);
 router.get('/quizzes/:quizId(\\d+)',       quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+
 
 
 module.exports = router;
