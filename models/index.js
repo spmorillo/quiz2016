f4131e329aa4e9623ec4f20335d2062da5d1e35b
spmorillo <sergipm11@hotmail.com>
2016-05-06 15:35:23 +0200

    Migraciones

--- models/index.js
+++ models/index.js
@@ -29,26 +29,6 @@ var sequelize = new Sequelize(url,
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
 
-// sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync()
-    .then(function() {
-        // Ya se han creado las tablas necesarias.
-        return Quiz.count()
-                .then(function (c) {
-                    if (c === 0) {   // la tabla se inicializa solo si está vacía
-                        return Quiz.bulkCreate([ {question: 'Capital de Italia',   answer: 'Roma'},
-                                                 {question: 'Capital de Portugal', answer: 'Lisboa'}
-                                              ])
-                                   .then(function() {
-                                        console.log('Base de datos inicializada con datos');
-                                    });
-                    }
-                });
-    })
-    .catch(function(error) {
-        console.log("Error Sincronizando las tablas de la BBDD:", error);
-        process.exit(1);
-    });
 
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
