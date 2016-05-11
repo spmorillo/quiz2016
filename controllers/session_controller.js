a5b2a914b03c9c2c486ca0db108dc8fde6765028
spmorillo <sergipm11@hotmail.com>
2016-05-11 16:28:32 +0200

    Roles y permisos

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,4 @@
+
 var models = require('../models');
 var Sequelize = require('sequelize');
 var url = require('url');
@@ -21,6 +22,51 @@ exports.loginRequired = function (req, res, next) {
         res.redirect('/session?redir=' + (req.param('redir') || req.url));
     }
 };
+
+// MW que permite gestionar solamente si el usuario logeado es admin.
+exports.adminRequired = function(req, res, next){
+
+    var isAdmin      = req.session.user.isAdmin;
+
+    if (isAdmin) {
+        next();
+    } else {
+      console.log('Ruta prohibida: el usuario logeado no es administrador.');
+      res.send(403);    }
+};
+
+// MW que permite gestionar un usuario solamente si el usuario logeado es:
+//   - admin 
+//   - o es el usuario a gestionar.
+exports.adminOrMyselfRequired = function(req, res, next){
+
+    var isAdmin      = req.session.user.isAdmin;
+    var userId       = req.user.id;
+    var loggedUserId = req.session.user.id;
+
+    if (isAdmin || userId === loggedUserId) {
+        next();
+    } else {
+      console.log('Ruta prohibida: no es el usuario logeado, ni un administrador.');
+      res.send(403);    }
+};
+
+// MW que permite gestionar un usuario solamente si el usuario logeado es:
+//   - admin
+//   - y no es el usuario a gestionar.
+exports.adminAndNotMyselfRequired = function(req, res, next){
+
+    var isAdmin      = req.session.user.isAdmin;
+    var userId       = req.user.id;
+    var loggedUserId = req.session.user.id;
+
+    if (isAdmin || userId === loggedUserId) {
+        next();
+    } else {
+      console.log('Ruta prohibida: no es el usuario logeado, ni un administrador.');
+      res.send(403);    }
+};
+
 
 
 /*
@@ -77,7 +123,7 @@ exports.create = function(req, res, next) {
             if (user) {
                 // Crear req.session.user y guardar campos id y username
                 // La sesión se define por la existencia de: req.session.user
-                req.session.user = {id:user.id, username:user.username,tiempo: new Date()};
+                req.session.user = {id:user.id, username:user.username, isAdmin:user.isAdmin, tiempo:new Date()};
 
                 res.redirect(redir); // redirección a redir
             } else {
--- routes/index.js
+++ routes/index.js
@@ -16,42 +16,60 @@ router.get('/author', function(req, res, next) {
   res.render('author');
 });
 
-// Autoload de rutas que usen :quizId
+
+// Autoload de parametros
 router.param('quizId', quizController.load);  // autoload :quizId
 router.param('userId', userController.load);  // autoload :userId
-router.param('commentId', commentController.load); // autoload :commentId
+router.param('commentId', commentController.load);  // autoload :commentId
 
-// Definición de rutas de cuenta
-router.get('/users',                    userController.index);   // listado usuarios
-router.get('/users/:userId(\\d+)',      userController.show);    // ver un usuario
-router.get('/users/new',                userController.new);     // formulario sign un
-router.post('/users',                   userController.create);  // registrar usuario
-router.get('/users/:userId(\\d+)/edit', sessionController.loginRequired, userController.edit);     // editar información de cuenta
-router.put('/users/:userId(\\d+)',      sessionController.loginRequired, userController.update);   // actualizar información de cuenta
-router.delete('/users/:userId(\\d+)',   sessionController.loginRequired, userController.destroy);  // borrar cuenta
 
 // Definición de rutas de sesion
 router.get('/session',    sessionController.new);     // formulario login
 router.post('/session',   sessionController.create);  // crear sesión
 router.delete('/session', sessionController.destroy); // destruir sesión
 
+
+// Definición de rutas de cuenta
+router.get('/users',                    userController.index);   // listado usuarios
+router.get('/users/:userId(\\d+)',      userController.show);    // ver un usuario
+router.get('/users/new',                userController.new);     // formulario sign un
+router.post('/users',                   userController.create);     // registrar usuario
+router.get('/users/:userId(\\d+)/edit', sessionController.loginRequired, 
+										sessionController.adminOrMyselfRequired, 
+										userController.edit);     // editar información de cuenta
+router.put('/users/:userId(\\d+)',      sessionController.loginRequired, 
+										sessionController.adminOrMyselfRequired, 
+										userController.update);   // actualizar información de cuenta
+router.delete('/users/:userId(\\d+)',   sessionController.loginRequired, 
+										sessionController.adminAndNotMyselfRequired, 
+										userController.destroy);  // borrar cuenta
+
 // Definición de rutas de /quizzes
 router.get('/quizzes.:format?',                     quizController.index);
 router.get('/quizzes/:quizId(\\d+).:format?',       quizController.show);
-router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
-router.get('/quizzes/new',                 sessionController.loginRequired, quizController.new);
-router.post('/quizzes',                    sessionController.loginRequired, quizController.create);
-router.get('/quizzes/:quizId(\\d+)/edit',  sessionController.loginRequired, quizController.edit);
-router.put('/quizzes/:quizId(\\d+)',       sessionController.loginRequired, quizController.update);
-router.delete('/quizzes/:quizId(\\d+)',    sessionController.loginRequired, quizController.destroy);
+router.get('/quizzes/:quizId(\\d+)/check', 	quizController.check);
+router.get('/quizzes/new',                 	sessionController.loginRequired, 
+											quizController.new);
+router.post('/quizzes',                    	sessionController.loginRequired, 
+											quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',  	sessionController.loginRequired, 
+										   	quizController.ownershipRequired, 
+										   	quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       	sessionController.loginRequired, 
+											quizController.ownershipRequired, 
+											quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    	sessionController.loginRequired, 
+											quizController.ownershipRequired, 
+											quizController.destroy);
 
-
-router.get('/quizzes/:quizId(\\d+)/comments/new',  sessionController.loginRequired, commentController.new);
-router.post('/quizzes/:quizId(\\d+)/comments',     sessionController.loginRequired, commentController.create);
+// Definición de rutas de comentarios
+router.get('/quizzes/:quizId(\\d+)/comments/new',  sessionController.loginRequired, 
+	                                               commentController.new);
+router.post('/quizzes/:quizId(\\d+)/comments',     sessionController.loginRequired, 
+	                                               commentController.create);
 router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', 
 	                                               sessionController.loginRequired, 
+	                                               quizController.ownershipRequired, 
 	                                               commentController.accept);
-
-
 
 module.exports = router;
